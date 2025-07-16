1e69f2df1f9e06c026b3217cf4d0efba65421c62
Hian Claudio <hclaudio@labsobralnet.ind>
2025-07-16 10:00:24 -0300

    produção

--- frontend/src/api/dashboard.js
+++
@@ -1,9 +0,0 @@
-import api from './axios'
-
-export const getDashboards = () => {
-  return api.get('/dashboards/')
-}
-
-export const getDashboardById = (id) => {
-  return api.get(`/dashboards/${id}/`)
-}
