3a60a1fe45c9e8402c8a2a6ff2a48ca2440c3fa2
Hian Claudio <hclaudio@labsobralnet.ind>
2025-07-18 10:36:39 -0300

    finalizado para producao 33

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import Cookies from 'js-cookie';
 
-const API_BASE_URL = import.meta.env.VITE_API_URL;
+const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://apibi.laboratoriosobral.com.br/api';
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
