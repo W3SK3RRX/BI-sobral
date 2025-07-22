8deda9f97ba8b011f7dd9cbb41f2df0642f345d3
Hian Claudio <hclaudio@labsobralnet.ind>
2025-07-22 10:21:37 -0300

    finalizado para producao 50

--- backend/dashboards/serializers.py
+++ backend/dashboards/serializers.py
@@ -1,26 +1,25 @@
 from rest_framework import serializers
 from .models import User, Category, Dashboard
-from rest_framework import serializers
 from django.utils import timezone
-
 from django.contrib.auth import get_user_model
+
 
 User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = (
-            "id",
-            "username",
-            "email",
-            "access_level",
-            "password",
-        )
-        extra_kwargs = {"password": {"write_only": True}}
+        fields = ("id", "username", "email", "access_level", "password")
+        extra_kwargs = {
+            "password": {"write_only": True},
+            "username": {"required": True},  # Força nome obrigatório
+        }
 
     def create(self, validated_data):
-        user = User.objects.create_user(**validated_data)
+        password = validated_data.pop("password")
+        user = User(**validated_data)
+        user.set_password(password)
+        user.save()
         return user
 
 class CategorySerializer(serializers.ModelSerializer):
