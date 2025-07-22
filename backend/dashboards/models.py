89b8d770f144f3827f267586f3afc442c44852d3
Hian Claudio <hclaudio@labsobralnet.ind>
2025-07-22 10:24:23 -0300

    finalizado para producao 51

--- backend/dashboards/models.py
+++ backend/dashboards/models.py
@@ -12,14 +12,11 @@ class User(AbstractUser):
         ('GESTOR', 'Gestor'),
         ('USUARIO', 'Usuário Comum'),
     )
-
     email = models.EmailField(unique=True)
     access_level = models.CharField(max_length=10, choices=USER_LEVELS, default='USUARIO')
     senha_alterada_em = models.DateTimeField(auto_now_add=True)
-    primeiro_acesso = models.BooleanField(default=True)
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']  # Agora username será usado apenas para exibição
+    primeiro_acesso = models.BooleanField(default=True)  # 👈 NOVO
 
     def senha_expirada(self):
         if self.access_level == 'ADMIN':
