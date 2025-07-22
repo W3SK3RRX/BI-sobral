a03f205a5bbdbf29a081defb42a01851c3c6b11f
Hian Claudio <hclaudio@labsobralnet.ind>
2025-07-21 22:00:57 -0300

    finalizado para producao 46

--- backend/dashboards/middleware/check_active_token.py
+++ backend/dashboards/middleware/check_active_token.py
@@ -3,8 +3,13 @@ from rest_framework_simplejwt.tokens import AccessToken
 from dashboards.models import ActiveSession
 from rest_framework.exceptions import AuthenticationFailed
 
+EXCLUDED_PATHS = ['/api/token/', '/api/token/refresh/']
+
 class ActiveSessionMiddleware(MiddlewareMixin):
     def process_request(self, request):
+        if any(request.path.startswith(p) for p in EXCLUDED_PATHS):
+            return
+
         auth_header = request.META.get('HTTP_AUTHORIZATION', '')
 
         if auth_header.startswith('Bearer '):
@@ -14,16 +19,10 @@ class ActiveSessionMiddleware(MiddlewareMixin):
                 access_token = AccessToken(token)
                 user_id = access_token['user_id']
 
-                # ✅ Verifica se a sessão ativa existe
                 session = ActiveSession.objects.filter(user_id=user_id).first()
                 if not session:
                     raise AuthenticationFailed("Sua sessão não é mais válida.")
-
-                # ✅ Garante que o token pertence ao Refresh ativo (extra segurança)
-                if str(token) not in session.refresh_token:
-                    raise AuthenticationFailed("Este token não é mais válido. Faça login novamente.")
-
             except AuthenticationFailed as e:
                 raise e
             except Exception:
-                pass  # Ignora se não for JWT válido
+                pass
