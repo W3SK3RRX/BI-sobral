e98edc96d94479ffbf06ef7d9f5549a0950d7c15
Hian Claudio <hclaudio@labsobralnet.ind>
2025-07-29 17:01:16 -0300

    finalizado para producao 73

--- frontend/src/components/FullscreenModal.jsx
+++ frontend/src/components/FullscreenModal.jsx
@@ -79,7 +79,7 @@ export const FullscreenModal = ({ isOpen, onClose, dashboardUrl, dashboardName }
                   flexWrap: 'wrap',
                   justifyContent: 'center',
                   alignContent: 'center',
-                  opacity: 0.03,
+                  opacity: 0.015,
                   fontSize: '2.5rem',
                   color: 'black',
                   transform: 'rotate(-30deg)',
