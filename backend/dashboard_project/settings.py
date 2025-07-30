327e90a9ddffb5d1200d79c8a63ad17df2ac4059
Hian Claudio <hclaudio@labsobralnet.ind>
2025-07-30 09:32:10 -0300

    finalizado para producao 74

--- backend/dashboard_project/settings.py
+++ backend/dashboard_project/settings.py
@@ -82,6 +82,20 @@ TEMPLATES = [
     },
 ]
 
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(hours=3),  # ou timedelta(hours=1), etc.
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
+    "ROTATE_REFRESH_TOKENS": False,
+    "BLACKLIST_AFTER_ROTATION": True,
+    "UPDATE_LAST_LOGIN": False,
+
+    "ALGORITHM": "HS256",
+    "SIGNING_KEY": SECRET_KEY,
+    "AUTH_HEADER_TYPES": ("Bearer",),
+    "AUTH_TOKEN_CLASSES": ("rest_framework_simplejwt.tokens.AccessToken",),
+    "TOKEN_TYPE_CLAIM": "token_type",
+}
+
 WSGI_APPLICATION = 'dashboard_project.wsgi.application'
 
 # Banco (você pode ajustar para PostgreSQL no docker-compose)
