5d902ff05b9746b0deb6bf323bd75dccf33198d1
Hian Claudio <hclaudio@labsobralnet.ind>
2025-08-06 14:57:18 -0300

    finalizado para producao 80

--- backend/dashboard_project/settings.py
+++ backend/dashboard_project/settings.py
@@ -84,7 +84,7 @@ TEMPLATES = [
 
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(hours=3),  # ou timedelta(hours=1), etc.
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
+    "REFRESH_TOKEN_LIFETIME": timedelta(hours=3),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": True,
     "UPDATE_LAST_LOGIN": False,
